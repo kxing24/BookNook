ca7f391e6b93f63fbdf828336956ad7cf9b258bf
Kathy Xing <kathyxing@fb.com>
2022-07-12 12:18:43 -0700

    added a splash screen

---
+++ app/src/main/java/com/codepath/kathyxing/booknook/activities/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.codepath.kathyxing.booknook.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.codepath.kathyxing.booknook.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        int SPLASH_TIMER = 3400;
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this , LoginActivity.class));
+                finish();
+            }
+        },SPLASH_TIMER);
+    }
+}
